76533932f82ee5840e14c16a347e0ff8f1e9ba3a
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2022-04-23 21:55:10 +0700

    find Product by ID

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -13,8 +13,8 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
-    final loadedProduct =
-        Provider.of<Products>(context, listen: false).findById(productId);
+    final loadedProduct = Provider.of<Products>(context).findById(productId);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
