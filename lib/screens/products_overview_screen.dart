4a6a8347b90f53fcd0726f5c71498e61fa9486c5
Thuan Nguyen <kean@Keans-MacBook-Pro.local>
2022-04-26 21:58:39 +0700

    update overview screen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/widgets/products_grid.dart';
 
+enum FilterOptions {
+  favorites,
+  all,
+}
+
 class ProductsOverviewScreen extends StatelessWidget {
   const ProductsOverviewScreen({Key? key}) : super(key: key);
 
@@ -11,6 +16,29 @@ class ProductsOverviewScreen extends StatelessWidget {
         title: const Text(
           'MyShop',
         ),
+        actions: <Widget>[
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                child: Text(
+                  'Only Favorites',
+                ),
+                value: FilterOptions.favorites,
+              ),
+              const PopupMenuItem(
+                child: Text(
+                  'Show All',
+                ),
+                value: FilterOptions.all,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.favorites) {
+              } else {}
+            },
+          ),
+        ],
       ),
       body: const ProductsGrid(),
     );
