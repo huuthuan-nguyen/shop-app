5d58ee9f71d3fb47e0483ba2419fe934d162e9c8
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2022-07-24 12:56:34 +0700

    handling authentication errors

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,6 +3,8 @@ import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:shop_app/models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   late String _token;
   late DateTime _expiryDate;
@@ -11,21 +13,35 @@ class Auth with ChangeNotifier {
   get returnSecureToken => null;
 
   Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, "signUp");
+  }
+
+  Future<void> signin(String email, String password) async {
+    return _authenticate(email, password, "signInWithPassword");
+  }
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     Uri url = Uri.https(
       "identitytoolkit.googleapis.com",
-      "/v1/accounts:signUp",
+      "/v1/accounts:$urlSegment",
       {
         "key": "AIzaSyDJGzllhk4U8aIe2C3TDdr0Y_foWprSAMM",
       },
     );
-    final response = await http.post(url,
-        body: json.encode({
-          "email": email,
-          "password": password,
-          "returnSecureToken": true,
-        }));
-    print(email);
-    print(password);
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            "email": email,
+            "password": password,
+            "returnSecureToken": true,
+          }));
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      rethrow;
+    }
   }
 }
